0186a98f1f64957124d0b288068493b88e739371
bibika <bibikakarki2021@gmail.com>
2022-01-29 14:10:41 +0545

    commit

---
+++ src/component/home/Home.jsx
@@ -0,0 +1,18 @@
+import "./home.css"
+// import { AddIcon } from '@material-ui/icons';
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div className="main">
+            <h1 className="1h"> Local search itahari</h1>
+            <div className="getL" title="get listed">
+                <button className="mBtn"> Get listed</button>
+               {/* <Add/> */}
+            </div>
+            
+        </div>
+    )
+}
+
+export default Home
